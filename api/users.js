2da1a68e6cae167fac194a9b0ab51fcc19d19818
swastica-s <swastica.s@pingbix.com>
2026-07-24 11:55:31 +0530

    Update users.js

--- api/users.js
+++ api/users.js
@@ -1,17 +1,6 @@
 export default function handler(req, res) {
   res.status(200).json({
-    success: true,
-    message: "API is working",
-    data: [
-      {
-        Number: "917502189959",
-        Name: "Swastica"
-      },
-      {
-        Number: "917042255531",
-        Name: "Nishant"
-      }
-    ]
+    name: "Swastica",
+    number: "917502189959"
   });
 }
-
